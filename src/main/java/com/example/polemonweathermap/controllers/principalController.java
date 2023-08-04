d0f5d04451e793a818b397293473952a852183e6
juanFRANvelilla <juanfran01velilla@gmail.com>
2023-08-04 13:36:41 +0200

    4-08-2023

--- src/main/java/com/example/polemonweathermap/controllers/principalController.java
+++ src/main/java/com/example/polemonweathermap/controllers/principalController.java
@@ -2,19 +2,14 @@ package com.example.polemonweathermap.controllers;
 
 import com.example.polemonweathermap.services.ForecastImageService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.io.File;
-import java.io.IOException;
-import java.net.http.HttpHeaders;
-import java.nio.file.Files;
-import java.nio.file.Path;
 
 @Controller
 public class principalController {
@@ -28,21 +23,15 @@ public class principalController {
     }
 
 
-    @PostMapping("/forecast")
-    public ResponseEntity<byte[]> handleForecast(@RequestParam("datePicker") String dateString) {
-        File image = forecastImageService.getImage(dateString);
-        if (image == null){
-            return ResponseEntity.notFound().build();
-        }
-        else {
-            try {
-                byte[] imageBytes = Files.readAllBytes(Path.of(image.getPath()));
 
-                return ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG).body(imageBytes);
-            } catch (IOException e) {
-                e.printStackTrace();
-                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-            }
+    @GetMapping(value = "/forecast", produces = MediaType.IMAGE_JPEG_VALUE)
+    public ResponseEntity<FileSystemResource> handleForecast(@RequestParam("datePicker") String dateString) {
+        File image = forecastImageService.getImage(dateString);
+        if (image == null) {
+            return ResponseEntity.notFound().build();
+        } else {
+            FileSystemResource imageResource = new FileSystemResource(image);
+            return ResponseEntity.ok().body(imageResource);
         }
     }
 
--- src/main/java/com/example/polemonweathermap/services/ForecastImageService.java
+++ src/main/java/com/example/polemonweathermap/services/ForecastImageService.java
@@ -1,23 +1,17 @@
 package com.example.polemonweathermap.services;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Objects;
-import java.util.stream.Collectors;
 
 @Service
 public class ForecastImageService {
 
-    //public ResponseEntity<byte[]> getImage(String selectedDate){
     public File getImage(String selectedDate){
         ArrayList<String> data = new ArrayList<>(Arrays.asList(selectedDate.split("-")));
 
-        // Ahora puedes acceder a los componentes de la fecha
         String year = data.get(0);
         String month = data.get(1);
         String day = data.get(2);
@@ -25,7 +19,6 @@ public class ForecastImageService {
 
         String path = "src\\main\\resources\\static\\images\\output\\spain\\" + year + "\\" + month + "\\" + day;
 
-        System.out.println("\n" + path);
         File directory = new File(path);
         if(directory.exists() && directory.isDirectory()){
             File[] fields = directory.listFiles();
@@ -34,7 +27,6 @@ public class ForecastImageService {
                 return image;
             }
         }
-        System.out.println("mal directorio");
         return null;
     }
 }
